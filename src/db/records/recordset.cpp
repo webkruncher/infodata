87b2065b456736825edb655ab3d83d2789ea4446
Jack M. Thompson <WebKruncher@GMail.com>
2022-01-01 19:59:14 +0000

    Crud

--- src/db/records/recordset.cpp
+++ src/db/records/recordset.cpp
@@ -53,42 +53,69 @@ using namespace StockMarket;
 namespace InfoMarketData
 {
 
-	struct Record : private DbRecords::RecordSetBase, StockMarket::TickerBase
-	{
-		void Create( const string symbol, const string name, const string market )
-		{
-			memset( &record, 0, sizeof( record ) );
-			SetName( name );
-			SetMarket( market );
-			SetLastUpdatedUTC( time( 0 ) );
-			DbRecords::RecordCreator<StockMarket::TickerBase> R( symbol, record, "./db/" );
-		}
-
-		void Update( const string symbol, const string name, const string market )
-		{
-			memset( &record, 0, sizeof( record ) );
-			SetName( name );
-			SetMarket( market );
-			SetLastUpdatedUTC( time( 0 ) );
-			DbRecords::RecordUpdater<StockMarket::TickerBase> R( symbol, record, "./db/" );
-		}
-
-		void Retrieve( const string symbol="" )
-		{
-			memset( &record, 0, sizeof( record ) );
-			DbRecords::RecordPrinter<StockMarket::TickerBase> R( cout, symbol, "./db/"  );
-		}
-	};
-
 	pair< unsigned char*,size_t > MarketData::operator()( const string& payload ) 
 	{	
-		string tickersymbol( "WIP" );
-		Record record;
-		if ( false )
+		pair< unsigned char*,size_t > empty( nullptr, 0 );
+		stringvector parts;
+		parts.split( payload, "|" );
+		const size_t npieces( parts.size() );
+		if ( npieces < 3 ) return empty;
+
+		int J( 1 );
+		const string method( parts[ J++ ] );
+		const string url( parts[ J++ ] );
+		const string ticker( parts[ J++ ] );
+		const string name( parts[ J++ ] );
+		const string market( parts[ J++ ] );
+		const string locale( parts[ J++ ] );
+		const string primary_exchange( parts[ J++ ] );
+		const string type( parts[ J++ ] );
+		const string active( parts[ J++ ] );
+		const string currency_name( parts[ J++ ] );
+		const string cik( parts[ J++ ] );
+		const string composite_figi( parts[ J++ ] );
+		const string share_class_figi( parts[ J++ ] );
+		const string last_updated( parts[ J++ ] );
+		cout << 
+			"method:" << method  << fence << "url:" << url  << fence << "ticker:" << ticker  << fence << "name:" << name  << fence << 
+			"market:" << market  << fence << "locale:" << locale  << fence << "primary_exchange:" << primary_exchange  << fence << "type:" << type  << fence << 
+			"active:" << active  << fence << "currency_name:" << currency_name  << fence << "cik:" << cik  << fence << "composite_figi:" << composite_figi  << fence << 
+			"share_class_figi:" << share_class_figi  << fence << "last_updated:" << last_updated  << fence << endl;
+
+
+
+		memset( &record, 0, sizeof( record ) );
+		SetName( name );
+		SetMarket( market );
+		SetLocale( locale );
+		SetPrimaryExchange( primary_exchange );
+		SetType( type );
+		SetActive( active );
+		SetCurrencyName( currency_name );
+		SetPrimaryCIK( cik );
+		SetCompositeFigi( composite_figi );
+		SetShareClassFigi( share_class_figi );
+		SetLastUpdatedUTC( last_updated );
+		DbRecords::RecordCreator<StockMarket::TickerBase> R( ticker, record, "./bdb/" );
+		const unsigned long status( R );
+		if ( R )
 		{
-			record.Update( tickersymbol, tickersymbol + string(" updated" ), "updr" );
-		} else {
-			record.Create( tickersymbol, tickersymbol + string(" created" ), "newr" );
+			cerr << "Cannot create, updating" << endl;
+			DbRecords::RecordUpdater<StockMarket::TickerBase> R( ticker, record, "./bdb/" );
+			const unsigned long status( R );
+			if ( R ) 
+			{
+				cerr << "Cannot update" << endl;
+				stringstream ssr;
+				ssr << "ERROR" << fence << payload;
+				const size_t Len( ssr.str().size() );
+				// TBD: Consider placement new / delete, Find ~/Info malloc
+				unsigned char* data=(unsigned char*) malloc( Len );
+				memset( data, 0, Len );
+				memcpy( (char*) data, ssr.str().c_str(), Len );
+				pair< unsigned char*,size_t > ret( data, Len );
+				return ret;
+			}
 		}
 
 		stringstream ssr;
--- src/db/site/infoservice.cpp
+++ src/db/site/infoservice.cpp
@@ -90,7 +90,7 @@ namespace InfoKruncher
 		payload.assign( (char*) PostedContent.data(), PostedContent.size() );
 
 		stringstream get;
-		get << "Processing:" << respond.method << fence << respond.resource << payload;
+		get << fence << respond.method << fence << respond.resource << payload;
 		const size_t Len( get.str().size() );
 
 		InfoMarketData::MarketData markets( respond.options );
